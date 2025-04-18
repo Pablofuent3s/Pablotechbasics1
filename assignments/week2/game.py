feb4f965ca024ff4d3e2b9e8c43b6ed33595403b
Pablofuent3s <pablofuuentess24@gmail.com>
2025-04-18 17:40:59 +0200

    entering of retirement place, 2nd user imput
    Signed-off-by: Pablofuent3s <pablofuuentess24@gmail.com>

--- assignments/week2/game.py
+++ assignments/week2/game.py
@@ -27,3 +27,15 @@ elif number <= 120:
     print("You need to contact the Guinness World Records! You're SO OLD! I respect that.")
 else:
     print("That's not a human age number!")
+time.sleep (1)
+
+print ("where do you want to retire?")
+place = input("Enter your retirement place (by continent): ")
+if place == Europe:
+    print (" The average life expectance in Europe is 74 years for men and 81 for women!")
+elif place == Oceania:
+    print ("The average life expectance in Europe is 77 years for men and 83 for women!")
+else:
+    print ("I reccomend you to retire in Europe or Oceania! they have better life expectancy")
+
+time.sleep (1.5)
